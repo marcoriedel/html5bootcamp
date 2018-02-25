f15357af398c96ba28daf71dc3be32a6637b2fe9
marcoriedel <marcoriedel@gmail.com>
2018-02-25 13:04:35 -0300

    Exercise 3

--- Topics/Topic 1 - JavaScript, and DOM APIs/script.js
+++ Topics/Topic 1 - JavaScript, and DOM APIs/script.js
@@ -5,3 +5,21 @@ window.onload = function preloader() {
  function alertMsg(){
  	alert("Alert Message");
  }
+
+function apiCall() {
+
+	var request = new XMLHttpRequest();
+	
+	request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+        if (request.status === 200) {
+            document.getElementById('hidden').innerHTML = request.responseText;
+        } 
+        else {
+            document.getElementById('hidden').className = 'apiError';
+        }
+    }
+	};
+	request.open("GET", 'http://api.icndb.com/jokes/random' , true);
+	request.send(null);
+}
